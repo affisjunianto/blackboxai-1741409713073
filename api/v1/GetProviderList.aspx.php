4e2c31328fd034c676cf84e4da3520b46c3bee1c
affis junianto <69682437+affisjunianto@users.noreply.github.com>
2025-03-15 17:23:25 +0700

    Add api/v1/GetProviderList.aspx.php 1742034204388

---
+++ api/v1/GetProviderList.aspx.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);//Atau
+error_reporting(E_ALL && ~E_NOTICE);
+require 'koneksi.php';
+header('Content-Type: application/json');
+
+if(isset($_GET['agent_token']) && isset($_GET['agent_code'])){
+    $agent_token = mysqli_real_escape_string($call, $_GET['agent_token']);
+    $agent_code = mysqli_real_escape_string($call, $_GET['agent_code']);
+    
+    $cek_op = mysqli_query($call, "SELECT * FROM tb_agent WHERE agent_token = '$agent_token' AND agent_code = '$agent_code'");
+    
+    if(empty($agent_token) || empty($agent_code)){
+        $res = array('status' => 'fail', 'msg' => 'Invalid Parameter');
+    } else if(mysqli_num_rows($cek_op) == 0){
+        $res = array('status' => 'fail', 'msg' => 'Agent Tidak Ada');
+    } else {
+        $data_op = mysqli_fetch_array($cek_op);
+        if($data_op['agent_status'] == "blocked"){
+            $res = array('status' => 'fail', 'msg' => 'Agent Blocked');
+        } else {
+            $cek_p = mysqli_query($call, "SELECT * FROM tb_provider");
+            while($data_p = mysqli_fetch_array($cek_p)){
+                $data[] = array(
+                    'provider_code' => $data_p['provider_code'],
+                    'provider_name' => $data_p['provider_name'],
+                    'provider_type' => $data_p['provider_type'],
+                    'provider_image' => $data_p['provider_image'],
+                    'provider_status' => $data_p['provider_status']);
+            }
+            $res = array('status' => 'success', 'provider' => $data, 'msg' => 'SUCCESS');
+        }
+    }
+} else {
+    $res = array('status' => 'fail', 'msg' => 'Invalid Parameter');
+}
+print json_encode($res , JSON_PRETTY_PRINT);
